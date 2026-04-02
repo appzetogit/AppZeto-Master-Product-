157eeb616a4d3e715ba27921364e8f57745f4262
vishal patel <vishalpatel581012@gmail.com>
2026-04-02 16:43:17 +0530

    refresh issue solve

--- Frontend/src/index.jsx
+++ Frontend/src/index.jsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
 import { Toaster } from 'sonner'
 import App from './app/App.jsx'
 import './shared/styles/global.css'
